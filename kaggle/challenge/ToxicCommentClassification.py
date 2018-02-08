3cc9b96cd7beed742e4d58e11bc2146defea59d3
ekansrm <ekansrm0001@163.com>
2018-02-08 14:35:17 +0800

    ToxicCommentClassification

---
+++ kaggle/challenge/ToxicCommentClassification.py
@@ -0,0 +1,21 @@
+import os
+
+_prefix = ['~', '.ml', 'kaggle', 'ToxicCommentClassification']
+
+
+def filepath(path: str):
+    return os.path.join(*_prefix, path)
+
+
+train_filename = 'train.csv'
+test_filename = 'test.csv'
+train_filepath = filepath(train_filename)
+test_filepath = filepath(test_filename)
+
+
+__all__ = ['train_filename', 'train_filepath', 'test_filename', 'test_filepath', 'filepath', ]
+
+if __name__ == '__main__':
+    print(train_filepath)
+    print(test_filepath)
+    print(filepath('my.csv'))
